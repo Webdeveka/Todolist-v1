f15ad418869287edee1569262043b7d8ff2fbd7e
Konstantin <webdeveka@gmail.com>
2022-11-17 11:48:36 +0300

    add more informations

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-let items = ["Buy Food", "Cook Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food"];
+const workItems = [];
 app.set('view engine', 'ejs'); 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+// основная страница
 app.get("/", (req, res) => {
 
     let today = new Date();
@@ -18,60 +18,36 @@ app.get("/", (req, res) => {
         day: "numeric",
         month: "long"
     };
-
-    // let currentDay = today.getDay();
     let day = today.toLocaleDateString("en-GB", options);
-    // if (currentDay === 6 || currentDay === 0) {
-    //     day = "weekend";
-    // } else {
-    //     day = "weekday";
-    // }
-    // if (currentDay === 1) {
-    //     day = "Monday!!!!!";
-    // } else if (currentDay === 2) {
-    //     day = "Thusday!!!!!";
-    // } else if (currentDay === 3) {
-    //     day = "Whendsday!!!!!";
-    // } else if (currentDay === 4) {
-    //     day = "Thursday!!!!!";
-    // } else if (currentDay === 5) {
-    //     day = "Friday!!!!!";
-    // } else if (currentDay === 6) {
-    //     day = "Saturday!!!!!";
-    // } else if (currentDay === 0) {
-    //     day = "Sunday!!!!!";
-    // }
-    // switch (currentDay) {   // можно сделать через switch
-    //     case 1:
-    //         day = "Monday!!!!!";
-    //     break;
-    //     case 2:
-    //         day = "Monday!!!!!";
-    //     break;
-
-    //     default:
-    //         break;
-    // }
     res.render("list", {listTitle: day, newListItems: items});
-    // res.send("Hello!");
 });
 
-app.post("/", (req, res) => {
+app.post("/", function(req, res) {
     let item = req.body.newItem;   // получаем значение введенное в input
-    items.push(item);
-    res.redirect("/");
-    //console.log(item);
+    
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
+// вторая страница
 app.get("/work", (req, res) => {
     res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
-app.post("/work", (req,res) => {
-    let item = req.body.newItem;
-    workItems.push(item);
-    res.redirect("/work");
+app.get("/about", (req, res) => {
+    res.render("about");
 });
+
+// app.post("/work", (req,res) => {
+//     let item = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect("/work");
+// });
 
 app.listen(3000, () => {
     console.log("Server started on port 3000.");
